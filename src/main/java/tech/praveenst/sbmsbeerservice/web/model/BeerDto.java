353083e64a616f48438b9fd6592abc7e515d208c
Praveen Thangavelu <praveen.thangavelu@venafi.com>
2021-01-17 11:29:45 -0800

    Data model for beer single and paged list . Closes #3
    Signed-off-by: Praveen Thangavelu <praveen.thangavelu@venafi.com>

---
+++ src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package tech.praveenst.sbmsbeerservice.web.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/***
+ * Created by
+ *   author: praveen.thangavelu
+ *   date: 2021-01-17T11:16:26Z
+ */
+
+@Data //this one gets getters and setters + equals and hashcode
+@NoArgsConstructor @AllArgsConstructor @Builder public class BeerDto {
+  //required
+  private String beerName;
+  private BeerStyleEnum beerStyle;
+  private Long upc;
+  private BigDecimal price;
+  //nullable - optional
+  private UUID id;
+  private Integer quantityOnHand;
+  private OffsetDateTime createdDate;
+  private OffsetDateTime lastModifiedDate;
+  private Integer version;
+
+}
+
---
+++ src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package tech.praveenst.sbmsbeerservice.web.model;
+
+public enum BeerStyleEnum {
+  LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
