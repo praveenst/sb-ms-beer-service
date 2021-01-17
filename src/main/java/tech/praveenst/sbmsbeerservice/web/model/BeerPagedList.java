c27cb65f13f0b59ef379a8ec6d8225d6bbf9ba36
Praveen Thangavelu <praveen.thangavelu@venafi.com>
2021-01-17 11:32:09 -0800

    Data model for beer single and paged list . Closes #3
    Signed-off-by: Praveen Thangavelu <praveen.thangavelu@venafi.com>

--- src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerPagedList.java
+++ src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerPagedList.java
@@ -5,6 +5,13 @@ import java.util.List;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+/***
+ * Created by
+ *   author: praveen.thangavelu
+ *   date: 2021-01-17T11:31:07Z
+ */
+
+
 public class BeerPagedList extends PageImpl<BeerDto> {
   public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
     super(content, pageable, total);
--- src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/tech/praveenst/sbmsbeerservice/web/model/BeerStyleEnum.java
@@ -1,5 +1,12 @@
 package tech.praveenst.sbmsbeerservice.web.model;
 
+/***
+ * Created by
+ *   author: praveen.thangavelu
+ *   date: 2021-01-17T11:31:19Z
+ */
+
+
 public enum BeerStyleEnum {
   LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
