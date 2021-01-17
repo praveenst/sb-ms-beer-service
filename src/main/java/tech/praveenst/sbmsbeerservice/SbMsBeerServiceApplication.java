e0a82bbce0e1c0860200ae3fc6b51ea0b925e8b3
Praveen Thangavelu <praveen.thangavelu@venafi.com>
2021-01-17 10:20:35 -0800

    Initial project add. Closes #1
    Signed-off-by: Praveen Thangavelu <praveen.thangavelu@venafi.com>

---
+++ src/main/java/tech/praveenst/sbmsbeerservice/SbMsBeerServiceApplication.java
@@ -0,0 +1,12 @@
+package tech.praveenst.sbmsbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SbMsBeerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbMsBeerServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/tech/praveenst/sbmsbeerservice/SbMsBeerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package tech.praveenst.sbmsbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SbMsBeerServiceApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
