6674358e2015760fbf7ec5cacdbed1375e5c81ea
Praveen Thangavelu <praveen.thangavelu@venafi.com>
2021-01-18 09:57:09 -0800

    controller tests for beer get, put and post . Closes #7
    Signed-off-by: Praveen Thangavelu <praveen.thangavelu@venafi.com>

---
+++ src/main/java/tech/praveenst/sbmsbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,36 @@
+package tech.praveenst.sbmsbeerservice.bootstrap;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import tech.praveenst.sbmsbeerservice.domain.Beer;
+import tech.praveenst.sbmsbeerservice.repositories.BeerRepository;
+
+@Component public class BeerLoader implements CommandLineRunner {
+
+  private final BeerRepository beerRepository;
+
+  public BeerLoader(BeerRepository beerRepository) {
+    this.beerRepository = beerRepository;
+  }
+
+
+  @Override public void run(String... args) throws Exception {
+    loadBeerObjects();
+  }
+
+  private void loadBeerObjects() {
+    if (beerRepository.count() == 0) {
+      beerRepository.save(
+          Beer.builder().beerName("Indian Pale Ale").beerStyle("PALE_ALE").quantityToBrew(2000).minOnHand(120)
+              .upc(337010000001L).price(new BigDecimal("12.88")).build());
+
+      beerRepository.save(
+          Beer.builder().beerName("Dog Fish ").beerStyle("IPA").quantityToBrew(2000).minOnHand(100).upc(337010000002L)
+              .price(new BigDecimal("19.98")).build());
+    }
+    // System.out.println("Beer count: " + beerRepository.count());
+  }
+}
--- src/main/java/tech/praveenst/sbmsbeerservice/domain/Beer.java
+++ src/main/java/tech/praveenst/sbmsbeerservice/domain/Beer.java
@@ -5,6 +5,7 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Version;
@@ -13,7 +14,13 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
-public class Beer {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder @Entity public class Beer {
   @Id @GeneratedValue(generator = "UUID") @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
   @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false) private UUID id;
 
